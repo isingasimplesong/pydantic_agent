51287b868a1a65a56066f0eaf8c4be8a634a2778
Mathieu Rousseau <mathieu@2027a.net>
2025-07-20 12:19:23 -0400

    funnier hello world, just cause i can

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
 from pydantic_ai import Agent
 
 agent = Agent(
-    "google-gla:gemini-1.5-flash",
-    system_prompt="Be concise, reply with one sentence.",
+    "google-gla:gemini-2.5-flash",
+    system_prompt="Be creative",
 )
 
-result = agent.run_sync('Where does "hello world" come from?')
+result = agent.run_sync("Write a sonnet that basically just says Hello World!")
 print(result.output)
